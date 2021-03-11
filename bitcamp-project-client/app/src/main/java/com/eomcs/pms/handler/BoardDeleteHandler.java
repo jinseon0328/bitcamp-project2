7feb1e781d505f22c57f009f6b068ea3f23df6dc
jinseon0328 <wishofsun@daum.net>
2021-03-11 17:44:29 +0900

    2021-03-11

--- bitcamp-project-client/app/src/main/java/com/eomcs/pms/handler/BoardDeleteHandler.java
+++ bitcamp-project-client/app/src/main/java/com/eomcs/pms/handler/BoardDeleteHandler.java
@@ -13,19 +13,6 @@ public class BoardDeleteHandler implements Command {
     int no = Prompt.inputInt("번호? ");
 
     stmt.executeQuery("board/select", Integer.toString(no));
-    out.writeUTF();
-    out.writeInt(1);
-    out.writeUTF(Integer.toString(no));
-    out.flush();
-
-    String status = in.readUTF();
-    in.readInt();
-    String data = in.readUTF();
-
-    if (status.equals("error")) {
-      System.out.println(in.readUTF());
-      return;
-    }
 
     //    // 서버에서 보낸 게시글을 사용할 일이 없기 때문에 리턴 값을 무시한다.
     //    // - 중요한 것은 서버가 출력했으면 클라이언트는 필요가 없더라도 읽어야 한다.
